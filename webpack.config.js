65c2d0f4b740c63ae231ee5992d8b5684b336aec
Zik <vladziku@gmail.com>
2022-04-26 11:41:14 +0300

    Enabled images loading from html. Reformat. Added gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,56 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry:{
-        main:'./src/index.js'},
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-        publicPath: ''
-        
-    },
-devServer:{
-    port: 4200
-},
-plugins:[
-new HtmlWebpackPlugin({
-    template:'./src/index.html'
-}),
-new CleanWebpackPlugin(),
-new MiniCssExtractPlugin()
-],
-module:{
+  mode: "development",
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: "",
+  },
+  devServer: {
+    port: 4200,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "./images", to: "images" }],
+    }),
+  ],
+  module: {
     rules: [
-        {
-          test: /\.js$/,
-          use: 'babel-loader',
-          exclude: '/node_modules/'
-        },
-        {
-          test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-          type: 'asset/resource'
-        },
-        {
-            test: /\.css$/,
-            use: [MiniCssExtractPlugin.loader, {
-              loader: 'css-loader',
-              options: {
-                importLoaders: 1
-              },
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
             },
-              'postcss-loader'
-            ]
           },
-        ]
-    }}
+          "postcss-loader",
+        ],
+      },
+    ],
+  },
+};
